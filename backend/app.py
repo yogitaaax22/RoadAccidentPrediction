7f476aac7aa460888f29c53bff59ce6774974f37
Yogita Maheshwari <yogitamaheshwari22@gmail.com>
2026-03-10 16:39:11 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -254,4 +254,5 @@ def predict():
 # Run Server
 # ==========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
